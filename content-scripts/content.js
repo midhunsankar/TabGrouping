c8ddba1517118d2078d4f845a6801637558e7f58
midhun <midhunsankar@live.com>
2024-10-27 23:30:22 +1300

    refacroring

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -1,9 +1,5 @@
+'use strict';
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("Message received", request, sender);
-  console.log(chrome.tabGroups);
-  // chrome.tabs.query({ active: false, currentWindow: true }, function(tabs) {
-  //   console.log("tabs", tabs);
-  // });
-
   sendResponse({ fromcontent: "This message is from content.js" });
 });
